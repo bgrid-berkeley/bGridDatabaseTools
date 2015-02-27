9e51f120c295929f476a95998889a0554a59ae85
mtabone <michaelangelo.tabone@gmail.com>
2015-02-27 09:10:02 -0800

    more comments

--- pgForecastIO/pgForecastIO_driverExample.R
+++ pgForecastIO/pgForecastIO_driverExample.R
@@ -1,21 +1,27 @@
+## Load required libraries and source the file with the database functions
 require(RPostgreSQL)
 require(Rforecastio)
 source('path/to/bGridDatabaseTools/pgForecastIO/pgForecastIO.R')
 
+## Setup thee postgresql driver
 drv <- dbDriver("PostgreSQL")
 
+## Load your api keys and passwords
 apikey   <- readLines('path/to/your/secret/apikeyfor/forecastio')[1]
 psqlpass <- readLines('path/to/your/secret/password/for/PostgreSQL')[1]
 
-## Connect to the consert database
+## Connect to the weather database
 wcon <- dbConnect(drv, user ="uname", pass = psqlpass, dbname="weather_forecastio", host="switch-db2.erg.berkeley.edu", port=5432)
 
-## get some data
+## Get some data
+# Set time bounds within which to collect data
 tbounds = as.POSIXct(c("2011-12-01 00:00:00" ,"2011-12-03 17:45:00"), tz = 'America/Regina')
 
+# set latitude and longitude
 lat <- 37
-lon <- -114
+lon <- -111
 
+# Get data at all costs!
 weatherdata <- FIOWeatherGetHourlyAtAllCosts(latitude = lat, longitude = lon, 
                                                timebounds = tbounds, dbcon = wcon,
                                                apikey = apikey, verbose = TRUE)

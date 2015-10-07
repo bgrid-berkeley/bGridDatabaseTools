6c9f5ca4030298ae4dcfeae7b3394cd26f59e4cc
mtabone <m.tabone@berkeley.edu>
2015-10-06 19:12:54 -0700

    adding documentation

--- dataDocumentation/rass/queries/dd65.sql
+++ dataDocumentation/rass/queries/dd65.sql
@@ -1,5 +1,4 @@
 ﻿-- View: rass.dd65
-
 -- DROP VIEW rass.dd65;
 
 CREATE OR REPLACE VIEW rass.dd65 AS 
@@ -11,13 +10,13 @@ CREATE OR REPLACE VIEW rass.dd65 AS
    FROM ( SELECT ddn_electricbillingdatamodels.t24cz,
             avg(ddn_electricbillingdatamodels.hddnorm) AS hddnorm
            FROM rass.ddn_electricbillingdatamodels
-          WHERE ddn_electricbillingdatamodels.heatref = 65 AND ddn_electricbillingdatamodels.t24cz > 1 OR ddn_electricbillingdatamodels.heatref >= 64 AND ddn_electricbillingdatamodels.heatref <= 66 AND ddn_electricbillingdatamodels.t24cz = 1
+          WHERE ddn_electricbillingdatamodels.heatref = 65 OR (ddn_electricbillingdatamodels.heatref >= 64 AND ddn_electricbillingdatamodels.heatref <= 66 AND ddn_electricbillingdatamodels.t24cz IN (1,5))
           GROUP BY ddn_electricbillingdatamodels.t24cz
           ORDER BY ddn_electricbillingdatamodels.t24cz) hdd65,
     ( SELECT ddn_electricbillingdatamodels.t24cz,
             avg(ddn_electricbillingdatamodels.hddnorm) AS hddnorm
            FROM rass.ddn_electricbillingdatamodels
-          WHERE ddn_electricbillingdatamodels.heatref = 72 AND ddn_electricbillingdatamodels.t24cz > 1 OR ddn_electricbillingdatamodels.heatref = 71 AND ddn_electricbillingdatamodels.t24cz = 1
+          WHERE ddn_electricbillingdatamodels.heatref = 72 OR (ddn_electricbillingdatamodels.heatref = 71 AND ddn_electricbillingdatamodels.t24cz IN (1,3))
           GROUP BY ddn_electricbillingdatamodels.t24cz
           ORDER BY ddn_electricbillingdatamodels.t24cz) hdd72,
     ( SELECT ddn_electricbillingdatamodels.t24cz,
@@ -28,5 +27,7 @@ CREATE OR REPLACE VIEW rass.dd65 AS
           ORDER BY ddn_electricbillingdatamodels.t24cz) cdd72
   WHERE cdd72.t24cz = hdd72.t24cz AND cdd72.t24cz = hdd65.t24cz;
 
-ALTER TABLE rass.dd65
-  OWNER TO bgrid;
+--ALTER TABLE rass.dd65
+ -- OWNER TO bgrid;
+
+SELECT * FROM rass.dd65;
--- dataDocumentation/rass/queries/heat_cool_backout.sql
+++ dataDocumentation/rass/queries/heat_cool_backout.sql
@@ -1,16 +1,18 @@
 ﻿DROP VIEW rass.heat_cool_backout;
 
 CREATE VIEW rass.heat_cool_backout AS
-SELECT a.ident, a.avginc, a.czt24, a.cecfast, a.sqft, a.sqft_a, a.homeage, a.remod, 
-	a.dwltype, a.stories,
+SELECT  a.ident, a.avginc, a.czt24, a.cecfast, 
+	a.sqft, a.sqft_a, a.homeage, a.remod, a.dwltype, a.stories,
 	a.nr0_5, a.nr6_18, a.nr19_34, a.nr35_54, a.nr55_64, a.nr65_99,
-	a.payheat, a.paycool, a.phtngfwl as groom, a.phtelbsb, a.phtelwhp, a.phtelpor,
+	a.payheat, 
+	a.paycool, a.phtngfwl as groom, a.phtelbsb, a.phtelwhp, a.phtelpor,
 	(a.phtelbsb = 1 OR a.phtelwhp =1  OR a.phtelpor =1) as eroom, 
 	a.windtype, a.noroomac, 
 	b.hdd65 as hdd65, b.cdd65 as cdd65,
 	a.new_cac_uec, a.new_eht_uec, a.new_auxht_uec, a.new_rac_uec, a.new_swp_uec,
         a.new_pmp_uec, a.new_ght_uec, a.new_gauxht_uec
 FROM rass.survdata AS a, rass.dd65 as b
+
 WHERE a.czt24 = b.t24cz;
 
 SELECT * FROM rass.heat_cool_backout;
